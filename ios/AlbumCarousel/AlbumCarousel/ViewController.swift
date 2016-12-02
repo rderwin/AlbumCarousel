43db31d645e2e1d642fbe82749b23bd343ab5937
Dave Erwin <davee@surveymonkey.com>
2016-12-02 00:12:23 -0800

    outlets

--- ios/AlbumCarousel/AlbumCarousel/ViewController.swift
+++ ios/AlbumCarousel/AlbumCarousel/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var artistLabel: UILabel!
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
