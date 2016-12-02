3966522c9aff3aa98981bb0bd538f274fde9dcc5
Dave Erwin <davee@surveymonkey.com>
2016-12-01 22:56:39 -0800

    create iOS project

---
+++ ios/AlbumCarousel/AlbumCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlbumCarousel
+//
+//  Created by Dave Erwin on 12/1/16.
+//  Copyright © 2016 Dave Erwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
